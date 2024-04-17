27359e3620dfe3f5f942da06013258a09ccca455
Sergij <stiv4113@gmail.com>
2024-04-18 00:55:08 +0300

    fixed the property visibility zones

--- Task2/Rectangle.cs
+++ Task2/Rectangle.cs
@@ -15,18 +15,18 @@ public class Rectangle
 {
     private double side1, side2;
     
-    Rectangle(double side1, double side2)
+   public Rectangle(double side1, double side2)
     {
         this.side1 = side1;
         this.side2 = side2;
     }
 
-    double Area
+    public double Area
     {
         get { return AreaCalculator(); }
     }
 
-    double Perimeter
+    public  double Perimeter
     {
         get { return PerimeterCalculator(); }
     }
